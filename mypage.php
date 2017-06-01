5ad6122a51e40ba95581c1e4960c4b8f69bfa6ca
Boniface <bonifs1@medtronic.com>
2017-06-01 14:48:19 -0500

    first commit

---
+++ mypage.php
@@ -0,0 +1,5 @@
+require_once 'vendor/autoload.php';
+use WindowsAzure\Common\ServicesBuilder;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=bonifs1storageacct;AccountKey=pUE+2jvsFESmzn7B37lCzdbzdDrXkmYrBA7fug0wOMv662yaHrBP3dmOZFf7sNq++rJh/XtuWQjmtkzcJJ2phg==;EndpointSuffix=core.windows.net";
+
